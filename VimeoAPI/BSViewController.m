d337ec8c77d2d2c30500d8458d5f36e8c2a07de9
Brian Semiglia <brian.semiglia@gmail.com>
2014-09-05 17:16:38 -0400

    Inserts error to prevent missing credentials.

--- VimeoAPI/BSViewController.m
+++ VimeoAPI/BSViewController.m
@@ -14,6 +14,7 @@
 #import "BSVimeoDeveloperCredentials.h"
 #import "NSUserDefaults+VimeoAccessCredentials.h"
 
+#error Make sure to define your key/secret.
 #define CONSUMER_KEY @""
 #define CONSUMER_SECRET @""
 
